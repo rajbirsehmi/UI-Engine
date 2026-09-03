6be68885aac4e0aa7e3f3ec069ac2f8a5c9b584b
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-09-03 16:37:10 -0700

    Fixed isMinifyEnabled value to make engine visible to consuming application

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -19,7 +19,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
+            isMinifyEnabled = false
             isShrinkResources = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
