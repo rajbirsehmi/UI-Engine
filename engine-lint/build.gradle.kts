c645361fa40034daf9d7cf515a8f4e96bda177b9
Rajbir Sehmi <raj.sehmi1@yahoo.com>
2026-09-07 22:39:09 -0700

    Bump version to 0.2.5-alpha and unify Group IDs

--- engine-lint/build.gradle.kts
+++ engine-lint/build.gradle.kts
@@ -39,9 +39,9 @@ configure<PublishingExtension> {
     publications {
         register<MavenPublication>("maven") {
             from(components["java"])
-            groupId = "com.sehmi"
+            groupId = "com.sehmi.engine"
             artifactId = "engine-lint"
-            version = "0.2.4-alpha"
+            version = "0.2.5-alpha"
         }
     }
 }
